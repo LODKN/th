197fbb7a396db275b82a28bf0b58e818ff4794fb
reKo <92667845+33xx@users.noreply.github.com>
2021-10-21 11:59:03 +0300

    Create البنك المتطور.py

---
+++ userbot/plugins/البنك المتطور.py
@@ -0,0 +1,67 @@
+# ======================================================================================================================================
+# ping -> edited ping with pic by  @RR7PP
+# كتابة الملف لسورس ماتركـس فقط ممنوع نسبه لنفسك
+# تخمط دليل فشلك اخمط وكول اني مطور 😂😂
+
+import os
+from datetime import datetime
+
+from userbot import jmthon
+
+from . import hmention, reply_id
+
+"""
+try:
+    from . import PING_PIC, PING_TEXT
+except:
+    pass
+"""
+plugin_category = "tools"
+
+PING_PIC = os.environ.get("PING_PIC") or (
+    "https://telegra.ph/file/d75e7e9310ea3fa8ab49b.mp4"
+)
+
+JM_TXT = os.environ.get("PING_TEXT") or "الخسارة بعد المقاومة تختلف عن الخسارة بدونها.. إنها نصر من نوع آخر لا يعرف معناه إلا من جربه. 🔥"
+
+
+@jmthon.ar_cmd(
+    pattern="بنك متطور$",
+    command=("بنك متطور", plugin_category),
+    info={
+        "header": "امر تجربه البوت اذا يشتغل ارسل  .بنك متطور فقط",
+        "option": "امر بنك المتطور كتابة  @DEOOUS",
+        "usage": [
+            "{tr}بنك متطور",
+        ],
+    },
+)
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    start = datetime.now()
+    cat = await edit_or_reply(
+        event, "<b><i>  ..♰ البــــنك ♰..  </b></i>", "html"
+    )
+    end = datetime.now()
+    await cat.delete()
+    ms = (end - start).microseconds / 1000
+    if PING_PIC:
+        caption = f"<b><i>{JM_TXT}<i><b>\n<code>┏━━━━━━━┓\n┃ ♰ : 𝖉𝖊𝖛 - @DEOOUS\n┃ ♰ : <b>{hmention}</b>\n<b>┃ ♰ : {ms}\n</b>┗━━━━━━━┛"
+        await event.client.send_file(
+            event.chat_id,
+            PING_PIC,
+            caption=caption,
+            parse_mode="html",
+            reply_to=reply_to_id,
+            link_preview=False,
+            allow_cache=True,
+        )
+    else:
+        await event.edit_or_reply(
+            event, "<code>يجـب اضـافة متـغير `PING_PIC`  اولا  f<code>", "html"
+        )
+
+
+# ======================================================================================================================================
