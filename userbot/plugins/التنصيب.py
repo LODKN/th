757469b1f5e09f0e0b66efb37c3000bebc7f0fba
reKo <92667845+33xx@users.noreply.github.com>
2021-10-21 16:40:25 +0300

    Update التنصيب.py

--- userbot/plugins/التنصيب.py
+++ userbot/plugins/التنصيب.py
@@ -5,5 +5,5 @@ from userbot.utils import admin_cmd
 async def _(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit(
-            "♰︙**مـرحبا بـك عزيـزي** \n♰︙رابط التنصيب - [اضغط هنا](http://2u.pw/btpnU)\n♰︙قناة السورس - @DEOOUS"
+            "♰︙**مـرحبا بـك عزيـزي** \n♰︙رابط التنصيب - [اضغط هنا](https://n9.cl/ReKo0)\n♰︙قناة السورس - @k4kk44"
         )
