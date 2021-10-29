6af3d178add650b5228eff7b427d13b4a0701dca
reKo <92667845+33xx@users.noreply.github.com>
2021-10-30 02:27:45 +0300

    Update بروفايلز.py

--- userbot/plugins/بروفايلز.py
+++ userbot/plugins/بروفايلز.py
@@ -27,7 +27,7 @@ autopic_path = os.path.join(os.getcwd(), "userbot", "original_pic.png")
 digitalpic_path = os.path.join(os.getcwd(), "userbot", "digital_pic.png")
 autophoto_path = os.path.join(os.getcwd(), "userbot", "photo_pfp.png")
 
-digitalpfp = Config.DIGITAL_PIC or "https://telegra.ph/file/63a826d5e5f0003e006a0.jpg"
+digitalpfp = Config.DIGITAL_PIC or "https://telegra.ph/file/3acfb2c07f8331fde4122.jpg"
 RR7PP = Config.TIME_JM or "♰"
 
 normzltext = "1234567890"
