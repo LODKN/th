1659e3d45d233218775ade5b7ce9f85457813515
reKo <92667845+33xx@users.noreply.github.com>
2021-11-03 00:04:36 +0300

    Create Nnnuu5.py

---
+++ userbot/plugins/Nnnuu5.py
@@ -0,0 +1,11 @@
+@bot.on(admin_cmd(pattern="حجره$"))
+async def gamez(event):
+    if event.fwd_from:
+        return
+    botusername = "@nuLnBoT"
+    noob = "@nuLnBoT keko"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob)
+    await tap[0].click(event.chat_id)
+    await event.delete()
