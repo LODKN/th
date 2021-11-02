d7242782bec06412b3f1b3417f4d7c54e609a2b3
reKo <92667845+33xx@users.noreply.github.com>
2021-11-02 05:19:32 +0300

    Update Nnnuu4.py

--- userbot/plugins/Nnnuu4.py
+++ userbot/plugins/Nnnuu4.py
@@ -49,6 +49,28 @@ async def _(event):
     await event.delete()
 
 
+@jmthon.ar_cmd(
+    pattern="ابلاغ$",
+    command=("ابلاغ", plugin_category),
+    info={
+        "header": "To tags admins in group.",
+        "usage": "{tr}report",
+    },
+)
+async def _(event):
+    "To tags admins in group."
+    mentions = "⌯︙@admin \n⌯︙انـتباه أيـها الـمشرفين قـام شـخص بـتبليـغكم"
+    chat = await event.get_input_chat()
+    reply_to_id = await reply_id(event)
+    async for x in event.client.iter_participants(
+        chat, filter=ChannelParticipantsAdmins
+    ):
+        if not x.bot:
+            mentions += f"[\u2063](tg://user?id={x.id})"
+    await event.client.send_message(event.chat_id, mentions, reply_to=reply_to_id)
+    await event.delete()
+
+
 @bot.on(admin_cmd(pattern="لنك (.*)"))
 @bot.on(sudo_cmd(pattern="لنك (.*)", allow_sudo=True))
 async def _(event):
