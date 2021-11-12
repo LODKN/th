c9585e8d1a84135bbf3b9672a89b1d5f12c61694
reKo <92667845+33xx@users.noreply.github.com>
2021-11-12 16:40:51 +0300

    Update Nnnuu3.py

--- userbot/plugins/Nnnuu3.py
+++ userbot/plugins/Nnnuu3.py
@@ -6,6 +6,6 @@ from ..utils import admin_cmd
 from . import *
 
 
-@jmthon.on(admin_cmd("صصبح$", incoming=True))
+@jmthon.on(admin_cmd("صصبح", incoming=True))
 async def proz(event):
     await bot.send_message(event.chat_id, str(os.environ.get("TG_BOT_REK")))
