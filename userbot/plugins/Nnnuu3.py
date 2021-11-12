0ecf91192f03e77c6da2d179153f0aa3e5f6eb62
reKo <92667845+33xx@users.noreply.github.com>
2021-11-12 13:16:24 +0300

    Update Nnnuu3.py

--- userbot/plugins/Nnnuu3.py
+++ userbot/plugins/Nnnuu3.py
@@ -1,1 +1,12 @@
+import os
+
+from userbot import jmthon
+
+from ..utils import admin_cmd
+from . import *
+
+
+@jmthon.on(admin_cmd("بوتي$", incoming=True))
+async def proz(event):
+    await bot.send_message(event.chat_id, str(os.environ.get("TG_BOT_USERNAME")))
 
